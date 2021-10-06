d23bc8bb9045e47395ebc1d4b1c75182c0e659b4
funtastt <asnsalavat@gmail.com>
2021-10-06 11:43:52 +0500

    Update README.md and Test.java locally

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        int a = 2; //test1
-        int b = 2;
-        int c = 34;
-        int d = 4;
-
-        //comment 2
-
-        System.out.println(a + b + c - 1);
+        System.out.println("Поменял тут кое-что");
     }
 }
